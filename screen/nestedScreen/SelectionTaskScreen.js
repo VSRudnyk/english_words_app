1aa46ff517702d118d357d829ffd8aba21918b97
VSRudnyk <v.rudnyk@gmail.com>
2023-01-19 17:33:32 +0200

    fix

--- screen/nestedScreen/SelectionTaskScreen.js
+++ screen/nestedScreen/SelectionTaskScreen.js
@@ -2,14 +2,14 @@ import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { useState } from 'react';
 import { Picker } from '@react-native-picker/picker';
 import { useGetWordsQuery } from '../../redux/wordsAPi';
-import { useGetWordsWithMistakesQuery } from '../../redux/wordsAPi';
+// import { useGetWordsWithMistakesQuery } from '../../redux/wordsAPi';
 import { Loader } from '../../src/Components/Loader';
 
 export const SelectionTaskScreen = ({ navigation }) => {
   const { data: words, isSuccess, isFetching } = useGetWordsQuery();
-  const { data: mistakes } = useGetWordsWithMistakesQuery();
+  // const { data: mistakes } = useGetWordsWithMistakesQuery();
   const [wordCount, setWordCount] = useState(10);
-  console.log(mistakes.data);
+
   if (isFetching) return <Loader />;
 
   const allWordsLength = words.data.length;
